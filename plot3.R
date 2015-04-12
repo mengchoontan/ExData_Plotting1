72f2068b67ffed34ff0c0cdfb8d7539543b66886
MC <mc@dapoltd.com>
2015-04-13 06:18:45 +0800

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,3 +1,7 @@
 with(mydatasubset, plot(Date, Sub_metering_2, col = 'red', type = "n"))
 with(mydatasubset, plot(Sub_metering_1,Date, ylab = "Energy Sub Metering", type = "n"))
 with(mydatasubset, lines(Date, Sub_metering_1, type = 'l', col = 'black') )
+with(mydatasubset, lines(Date, Sub_metering_2, type = 'l', col = 'red') )
+with(mydatasubset, lines(Date, Sub_metering_3, type = 'l', col = 'blue') )
+legend("topright", lty = 1, col = c("black", "red", "blue"),
+legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
